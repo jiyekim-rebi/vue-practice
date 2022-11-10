c39cb20d0851c756de4d8dd653688e3731106b55
jiye.kim <jiye.kim@callgate.co.kr>
2022-11-10 15:14:31 +0900

    2022.11.10 vue 연습

---
+++ src/views/DataBinding.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>hello, {{title}}</h1>
+</template>
+
+<script>
+export default {
+
+  //arrow function
+  /*
+  data: () => ({
+    title: 'binding'
+  })
+   */
+
+  data () {
+    return {
+      //아 거참 겁나 까칠하네 ㅡㅡㅋㅋㅋㅋㅋ
+      title: 'binding'
+    }
+  }
+}
+</script>
